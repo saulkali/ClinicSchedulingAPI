191c5711a75c020d1d3957905bbec8b20ddabc9d
jupiter <jupiter@gmail.com>
2026-03-23 22:49:36 -0600

    feat: reparando pipline

--- ClinicScheduling.Api/obj/Debug/net9.0/ClinicScheduling.Api.AssemblyInfo.cs
+++ ClinicScheduling.Api/obj/Debug/net9.0/ClinicScheduling.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicScheduling.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c77bc72d5da461156242dc525835ff0a6ab5c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b81e9e1fdfa147f63aa9720dc2474f81c3e8d6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicScheduling.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicScheduling.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
